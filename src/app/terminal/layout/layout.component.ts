8df3e0fc06a342b83097befb35dd61aebcefa7ef
Anthony <61360659+Anthony-J-G@users.noreply.github.com>
2023-08-28 16:49:17 -0400

    Toolkit Tags Added As Well

--- src/app/terminal/layout/layout.component.ts
+++ src/app/terminal/layout/layout.component.ts
@@ -1,5 +1,17 @@
 import { Component } from '@angular/core';
+import { min } from 'rxjs';
 import * as data from 'src/assets/projects.json';
+
+const ICON_PATH: string = "assets/icons/";
+const ICONS: any = {
+  "Angular": "angular-icon-logo-png-transparent-3794980330.png",
+  "C++": "C-Logo.png",
+  "Unreal Engine": "UnrealEngineIcon.png",
+  "Python": "570-5708783_logo-python-python-logo-clipart.png",
+  "Django": "topic_icon_django-tutorials_44346a.png",
+  "SQLite": "SQLite_Vector_logo.png",
+  "TypeScript": "typescript_logo-1229761782.png"
+}
 
 
 @Component({
@@ -9,21 +21,35 @@ import * as data from 'src/assets/projects.json';
 })
 export class LayoutComponent {
   // Constants
-  SPLASH_PATH: string = "assets/splashes/"
+  SPLASH_PATH: string = "assets/splashes/";
 
-  count: number = 0;
+  index: number = 0;
   projects: any[] = data;
   currentProject: any = this.projects[0];
+  currentTags: string[] = [];
+
+  constructor() {
+    this.updateProject();
+  }
 
   onSelect(t: number) {
-    var index = (t + this.count) % this.projects.length;
-    this.count = index;
+    var count = (t + this.index);
+    if (count < 0) {
+      count += this.projects.length;
+    }
+    this.index = count % this.projects.length;
     this.updateProject()
 
   }
 
   updateProject(): void {
-    this.currentProject = this.projects[this.count];
+    this.currentProject = this.projects[this.index];
+    this.currentTags = [];
+    for (let toolkit of this.currentProject.toolkits) {
+      const icon = ICON_PATH + ICONS[toolkit]
+      this.currentTags.push(icon)
+    }
+
 
   }
 
